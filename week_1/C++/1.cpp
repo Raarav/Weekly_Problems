2cc0ea15598d86e8f4c3d322992578b2a35168c8
rawkush <ankushrawat950@gmail.com>
2018-08-15 14:42:07 +0530

    added  logical part

--- week_1/C++/1.cpp
+++ week_1/C++/1.cpp
@@ -10,9 +10,11 @@ int matrix[][]={{1,2,3},{2,3,4},{4,5,6}}; // assuming this is the given matrix
 //TODO: if needed input the matrix from user
 int firstD=0, secondD=0;
 
-for(int i=0, i<3; i++)
+for(int i=0; i<3; i++)
 {
 
+firstD=firstD+matrix[i][i];
+secondD=secondD+matrix[i][2-i];
 
 }
 
