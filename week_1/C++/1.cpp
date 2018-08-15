e13e7d46af10d15cf11b0bb2bfbdfbfc01e95fe5
rawkush <ankushrawat950@gmail.com>
2018-08-15 14:38:40 +0530

    created nested loops

--- week_1/C++/1.cpp
+++ week_1/C++/1.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 int main(){
 
+int matrix[][]={{1,2,3},{2,3,4},{4,5,6}}; // assuming this is the given matrix
 
+//TODO: if needed input the matrix from user
+int firstD=0, secondD=0;
+
+for(int i=0, i<3; i++)
+{
+
+    for(int j=0;j<3;j++){
+
+
+    }
+
+}
 
 
 return 0;
