e7e96b3758ac43ee9f70f7f7e9836d4363666612
rawkush <ankushrawat950@gmail.com>
2018-08-15 18:20:11 +0530

    added comments

--- week_1/C/1.c
+++ week_1/C/1.c
@@ -8,11 +8,11 @@ int matrix[3][3]={{1,2,9},{2,3,4},{4,5,6}}; // assuming this is the given matrix
 //TODO: if needed input the matrix from user
 int firstD=0, secondD=0;
 int i=0;
-for( i=0; i<3; i++)
+for( i=0; i<3; i++) // to do change the constant 3 with size of matrix
 {
 
 firstD=firstD+matrix[i][i];
-secondD=secondD+matrix[i][2-i];
+secondD=secondD+matrix[i][2-i]; // change 2 with number of column-1
 
 }
 printf("sum of first diagonal is %d",firstD);
