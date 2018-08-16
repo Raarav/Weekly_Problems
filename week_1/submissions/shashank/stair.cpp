2a7f20ce4d60bd9aa866734716e8d93ab4bb9faa
s-uniyal <shashankuniyal36@gmail.com>
2018-08-17 03:58:21 +0530

    staircase is done

---
+++ week_1/submissions/shashank/stair.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,i,j,k; 
+    cout<<"no of steps:";
+    cin>>n;
+
+    for(k=0;k<n;k++)
+    {
+      //for spaces
+    for(i=n-1;i>k;i--)
+    {
+      cout<<" ";
+    } 
+    // for 
+    for(j=0;j<k+1;j++)
+    cout<<"*";
+    //back to the loop of k and n
+    cout<<"\n";
+    }   
+    cout<<"done! bitches😉";
+    return 0;
+}
