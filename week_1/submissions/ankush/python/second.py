42b5bfa11f6f3ef0267600735ceef8524a5f44be
rawkush <ankushrawat950@gmail.com>
2018-08-17 17:16:07 +0530

    changed the loops for convienience

--- week_1/submissions/ankush/python/second.py
+++ week_1/submissions/ankush/python/second.py
@@ -1,14 +1,11 @@
 n=int(input("enter the number of lines "))
 i=0
 j=0;
-while i<n:
+for i in range(0, n):
     j=1
-    while j<n:
+    for j in range(0, n):
         print(" ")
-        j=j+1
-    j=1
-    while j<=i:
+     
+    for j in range(0, i+1):
         print("#")
-        j=j+1
-    i=i+1
 
