76cc278514afa98ebd25d53d02d3b4f5c83eb0b7
s-uniyal <shashankuniyal36@gmail.com>
2018-08-18 02:39:23 +0530

    added an error func to exit from program if time formatting goes wrong.

--- week_1/submissions/shashank/Time-has-come.cpp
+++ week_1/submissions/shashank/Time-has-come.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
+
+void error(int hr, int min, int sec){
+  if(hr>12||min>60 || sec>60)
+  cout<<"Learn time formats.";
+    exit(0);
+}
+
+
 int main() {
   string mode;
   int hr,min,sec;
   cin>>hr>>min>>sec;
-  cout<<"Time in 12 hr format:";
+error(hr,min,sec);
+  cout<<"⌛⏲ Time in 12 hr format:";
   cout<<hr<<":"<<min<<":"<<sec<<endl;
   cout<<"choose am or pm:";
   cin>>mode;
@@ -15,20 +25,22 @@ int main() {
       hr=0;
 
     }
-cout<<"Time in 24 hr format:\n";
+cout<<"⌚⏳ Time in 24 hr format:\n";
     cout<<hr<<":"<<min<<":"<<sec<<endl;
   }
   else if(mode =="pm" || mode=="PM"){
         if(hr==12){
-            cout<<"Time in 24 hr format:\n";
+            cout<<"⌚⏳ Time in 24 hr format:\n";
     cout<<hr<<":"<<min<<":"<<sec<<endl;
         }
         else{
             hr+= 12;
-    cout<<"Time in 24 hr format:\n";
+    cout<<" ⌚⏳ Time in 24 hr format:\n";
     cout<<hr<<":"<<min<<":"<<sec<<endl;
         }
 
   }
+  cout<<"⏳ Time to end the execution.";
   return 0;
 }
+
