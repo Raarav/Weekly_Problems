9e80d5c1b976761057864c8b38c3d2fe4f8c7608
rawkush <ankushrawat950@gmail.com>
2018-08-17 17:20:22 +0530

    completed

--- week_1/submissions/ankush/python/second.py
+++ week_1/submissions/ankush/python/second.py
@@ -1,11 +1,10 @@
 n=int(input("enter the number of lines "))
-i=0
-j=0;
-for i in range(0, n):
-    j=1
-    for j in range(0, n):
-        print(" ")
-     
-    for j in range(0, i+1):
-        print("#")
 
+for i in range(0, n):
+    
+    for j in range(i, n):
+        print(" ",end="")     
+    for j in range(0, i+1):
+        print("#",end="")
+
+    print("\r")
