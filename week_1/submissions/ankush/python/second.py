1d8894eb37274d20edbef002d2db02d4536d8728
rawkush <ankushrawat950@gmail.com>
2018-08-17 17:10:59 +0530

    2 soltion in python

---
+++ week_1/submissions/ankush/python/second.py
@@ -0,0 +1,14 @@
+n=int(input("enter the number of lines "))
+i=0
+j=0;
+while i<n:
+    j=1
+    while j<n:
+        print(" ")
+        j=j+1
+    j=1
+    while j<=i:
+        print("#")
+        j=j+1
+    i=i+1
+
